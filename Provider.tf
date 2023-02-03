f10ce6b011f63cc975289864b67d5fa70a9d2cbd
Harsha <harshatummala@Harshas-MacBook-Air.local>
2023-02-03 11:37:15 -0500

    Comitted 3 files

---
+++ Provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = "dea5b60a-6dbe-4599-adf8-d1adfa1c3c07"
+  client_id       = "438d91d1-a180-4e8f-bf62-e34a330c11fe"
+  client_secret   = "dT28Q~qPZ7HDCW8tlO-0iZQWxMSakjF.Y-v2MaMV"
+  tenant_id       = "6f33a5a0-0cf2-433c-ae07-98c20761b9fe"
+}
---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "example" {
+  name     = "Resourcegroup1"
+  location = "West Europe"
+}
